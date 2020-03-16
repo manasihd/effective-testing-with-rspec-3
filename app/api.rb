b53eac495201ea58eb22f5a4c314a92e0746b93b
manasi <manasi@hotdoc.com.au>
2020-03-16 17:15:23 +1100

    Added real Ledger object

--- app/api.rb
+++ app/api.rb
@@ -2,7 +2,7 @@
 
 require 'sinatra/base'
 require 'json'
-
+require_relative 'ledger'
 module ExpenseTracker
   class API < Sinatra::Base
     def initialize(ledger: Ledger.new)
--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -4,7 +4,6 @@ require_relative '../../../app/api'
 require 'rack/test'
 
 module ExpenseTracker
-  RecordResult = Struct.new(:success?, :expense_id, :error_message)
 
   RSpec.describe API do
     include Rack::Test::Methods
